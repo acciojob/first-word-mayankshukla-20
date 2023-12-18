0e04767d0828bec9b663e7bac5b928a3b69fe4e2
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2023-12-18 13:45:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
   // your code here
+	let i = 0;
+	let ans = '';
+	while(i<s.length && s[i] != ' '){
+		ans += s[i];
+		i++;
+	}
+	return ans;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
